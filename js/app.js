c4412fece891aa72ee3c9fb4757dbbceee632f8c
Hamza Khalid <33630482+hamzakalid@users.noreply.github.com>
2021-01-23 01:38:37 +0300

    Upload the Project hamza khaled

---
+++ js/app.js
@@ -0,0 +1,46 @@
+$(function(){
+    $('.product-owl  .owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        // responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                // nav:true
+            },
+            600:{
+                items:3,
+                // nav:false
+            },
+            1000:{
+                items:5,
+                // nav:true,
+                // loop:false
+            }
+        }
+     });
+     
+
+     $('.testimonials .owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        items:1
+    });
+
+
+     $('.menu-icon').on('click',function(){
+        $('.menu').toggleClass('show');
+     });
+
+     //Mixup start
+     
+     AOS.init({
+        duration: 600,
+        easing: 'ease-in-sine',
+        dataAosOnce:true
+     });
+});
